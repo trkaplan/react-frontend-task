dd95f7e0a1ba07a228a8d0ad10cd5555789b6f40
Tuncay Kaplan <tuncaykmail-turkey@yahoo.com>
2019-01-14 11:46:58 +0300

    configure eslint + prettier + airbnb style guide

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Welcome!</h1>
-  </div>
+      </div>
     );
   }
 }
