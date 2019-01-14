5e7b1b5f9c9eb8e7259b87cb46f42e1d477e215a
Tuncay Kaplan <tuncaykmail-turkey@yahoo.com>
2019-01-14 14:27:44 +0300

    configure Prettier to omit semicolons

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
 
 class App extends Component {
   render() {
@@ -6,7 +6,7 @@ class App extends Component {
       <div>
         <h1>Welcome!</h1>
       </div>
-    );
+    )
   }
 }
-export default App;
+export default App
