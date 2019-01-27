a0c33abe5000ab64278cab87b10695ebb343cd21
Tuncay Kaplan <tuncaykmail-turkey@yahoo.com>
2019-01-28 00:18:59 +0300

    add hover state/style to UIKit/Chart Points

--- src/components/UIKit/Chart/Point.js
+++ src/components/UIKit/Chart/Point.js
@@ -1,19 +1,40 @@
-import React from "react"
+import React, { Component } from "react"
 import PropTypes from "prop-types"
 
-const Point = ({ d, onMouseEnter, onMouseLeave, onClick }) => (
-  <circle
-    cx={d.x}
-    cy={d.y}
-    r="4"
-    fill="white"
-    stroke="steelBlue"
-    strokeWidth="2.5"
-    onMouseEnter={() => onMouseEnter(d)}
-    onClick={() => onClick(d)}
-    onMouseLeave={() => onMouseLeave(d)}
-  />
-)
+class Point extends Component {
+  state = { isHover: false }
+
+  onMouseEnterHandler = (d, e, onMouseEnter) => {
+    onMouseEnter(d, e)
+    this.setState({ isHover: true })
+  }
+
+  onMouseLeaveHandler = (d, onMouseLeave) => {
+    onMouseLeave(d)
+    this.setState({ isHover: false })
+  }
+
+  render() {
+    const { d, onMouseEnter, onMouseLeave, onClick } = this.props
+    const { isHover } = this.state
+    const fillColor = isHover ? "steelBlue" : "white"
+    return (
+      <circle
+        cx={d.x}
+        cy={d.y}
+        r="4"
+        fill={fillColor}
+        style={{ cursor: "pointer" }}
+        stroke="steelBlue"
+        strokeWidth="2.5"
+        onMouseEnter={e => this.onMouseEnterHandler(d, e, onMouseEnter)}
+        onClick={() => onClick(d)}
+        onMouseLeave={() => this.onMouseLeaveHandler(d, onMouseLeave)}
+      />
+    )
+  }
+}
+
 Point.defaultProps = {
   onClick: () => {}
 }
